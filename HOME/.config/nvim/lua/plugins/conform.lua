d2fd138012514e56fca2c1626bcc4b1c5aff6be5
George Tom <georg3tom@gmail.com>
2024-03-30 16:13:21 +0530

    async on timeout

--- HOME/.config/nvim/lua/plugins/conform.lua
+++ HOME/.config/nvim/lua/plugins/conform.lua
@@ -1,3 +1,4 @@
+local slow_format_filetypes = {}
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
@@ -31,8 +32,27 @@ return {
 			css = { { "prettierd", "prettier" } },
 			markdown = { { "prettierd", "prettier" } },
 		},
+
 		-- Set up format-on-save
-		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		format_on_save = function(bufnr)
+			if slow_format_filetypes[vim.bo[bufnr].filetype] then
+				return
+			end
+			local function on_format(err)
+				if err and err:match("timeout$") then
+					slow_format_filetypes[vim.bo[bufnr].filetype] = true
+				end
+			end
+
+			return { timeout_ms = 200, lsp_fallback = false }, on_format
+		end,
+
+		format_after_save = function(bufnr)
+			if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+				return
+			end
+			return { lsp_fallback = true }
+		end,
 		-- Customize formatters
 		formatters = {
 			shfmt = {
