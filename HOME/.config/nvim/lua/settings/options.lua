161ff207c59fcb4a97ca4b2ca7ca6e27b185a0a4
George Tom <georg3tom@gmail.com>
2024-03-31 00:38:13 +0530

    nosplit on substitute

--- HOME/.config/nvim/lua/settings/options.lua
+++ HOME/.config/nvim/lua/settings/options.lua
@@ -45,7 +45,7 @@ vim.opt.list = true
 vim.opt.listchars = { tab = "  ", eol = "↲" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
+vim.opt.inccommand = "nosplit"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
