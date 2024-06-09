61b38472d046c318fae00ea7df098aaaaf150f83
George Tom <georg3tom@gmail.com>
2024-06-09 19:55:18 +0530

    option to disable format_on_save

--- HOME/.config/nvim/lua/plugins/conform.lua
+++ HOME/.config/nvim/lua/plugins/conform.lua
@@ -1,4 +1,23 @@
 local slow_format_filetypes = {}
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
+
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
@@ -35,6 +54,9 @@ return {
 
 		-- Set up format-on-save
 		format_on_save = function(bufnr)
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
 			if slow_format_filetypes[vim.bo[bufnr].filetype] then
 				return
 			end
