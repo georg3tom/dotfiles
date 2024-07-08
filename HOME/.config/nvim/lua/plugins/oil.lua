68ac48e335214a63b46028ec4850ac1e8dd16c33
George Tom <georg3tom@gmail.com>
2024-07-09 01:00:06 +0530

    oil conf

--- HOME/.config/nvim/lua/plugins/oil.lua
+++ HOME/.config/nvim/lua/plugins/oil.lua
@@ -4,7 +4,12 @@ return {
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("oil").setup({})
+		require("oil").setup({
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+			},
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
--- HOME/.config/nvim/lua/settings/maps.lua
+++ HOME/.config/nvim/lua/settings/maps.lua
@@ -7,7 +7,6 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-vim.keymap.set("n", "<leader><leader>", ":b#<CR>", { desc = "Switch to the last buffer" })
 
 -- Clear highlight on pressing <Esc> in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
