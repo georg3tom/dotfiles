0e2242f7ba17f4530c5143ed68e1e779975c1901
George Tom <georg3tom@gmail.com>
2024-07-09 01:45:13 +0530

    change maps

--- HOME/.config/nvim/lua/plugins/telescope.lua
+++ HOME/.config/nvim/lua/plugins/telescope.lua
@@ -84,7 +84,6 @@ return {
 
 		vim.keymap.set("n", "<leader>/", function()
 			require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
 				previewer = false,
 			}))
 		end, { desc = "[/] Fuzzily search in current buffer" })
@@ -101,7 +100,6 @@ return {
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { desc = "[S]earch [S]ymbols" })
-		vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", { desc = "[F]ile [B]rowse" })
 
 		vim.keymap.set("n", "<leader>b", function()
 			require("telescope.builtin").buffers({
--- HOME/.config/nvim/lua/settings/maps.lua
+++ HOME/.config/nvim/lua/settings/maps.lua
@@ -2,11 +2,5 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<C-j>", ":cnext<CR>", opts)
 vim.keymap.set("n", "<C-k>", ":cprev<CR>", opts)
 
--- Move to window using the <ctrl> hjkl keys
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- Clear highlight on pressing <Esc> in normal mode
+--- Clear highlight on pressing <Esc> in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
