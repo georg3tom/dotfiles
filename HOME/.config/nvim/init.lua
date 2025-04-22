787c5c507253627cb08f6de3a6aaa9116721f07a
George Tom <georg3tom@gmail.com>
2025-04-23 00:24:40 +0530

    nvim rose pine

--- HOME/.config/nvim/init.lua
+++ HOME/.config/nvim/init.lua
@@ -1,20 +1,20 @@
-require('settings')
+require("settings")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
-  install = { colorscheme = { "catppuccin-frappe" } },
-  change_detection = {
-    notify = false,
-  },
+	install = { colorscheme = { "rose-pine" } },
+	change_detection = {
+		notify = false,
+	},
 })
-require('lsp')
+require("lsp")
